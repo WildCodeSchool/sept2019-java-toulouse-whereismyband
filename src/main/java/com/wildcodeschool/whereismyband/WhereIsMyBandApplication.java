2147bbaf017cf55eba2e6dc57c11a5e5f5115a9b
bastienwcs <email>
2019-10-18 13:19:16 +0200

    init

---
+++ src/main/java/com/wildcodeschool/whereismyband/WhereIsMyBandApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.whereismyband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsMyBandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsMyBandApplication.class, args);
+	}
+
+}
