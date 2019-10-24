71a66d1a501a9166d225bab30c151dc0d4d80a69
0_Nocaudie <o.nocaudie@googlemail.com>
2019-10-24 13:41:40 +0200

    Routes : finalisation Fragments : intégration du connect header et du welcome Bugfix : taille des polices du connect header

--- src/main/java/com/wildcodeschool/whereismyband/controller/ConnectedController.java
+++ src/main/java/com/wildcodeschool/whereismyband/controller/ConnectedController.java
@@ -1,11 +1,12 @@
 package com.wildcodeschool.whereismyband.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ConnectedController {
-    @PostMapping("/userProfile")
+    @GetMapping("/userProfile")
     public String toProfile() {
         return "userProfile";
     }
