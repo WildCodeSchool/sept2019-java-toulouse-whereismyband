a39aec8a383d9b3beaeab2edb30118eb8c2bab16
0_Nocaudie <o.nocaudie@googlemail.com>
2019-10-24 13:12:23 +0200

    bugfix: ajout des liens vers les polices quand ça manquait dans le html en attente du header connecté pour finaliser les fragments/routes

--- src/main/java/com/wildcodeschool/whereismyband/controller/ConnectedController.java
+++ src/main/java/com/wildcodeschool/whereismyband/controller/ConnectedController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ConnectedController {
     @PostMapping("/userProfile")
-    public String search() {
+    public String toProfile() {
         return "userProfile";
     }
+
+    @PostMapping("/search")
+    public String toSearch() {
+        return "search";
+    }
 }
+
