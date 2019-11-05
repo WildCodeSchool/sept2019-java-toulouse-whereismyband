0ca96a6db597a58f9030e0924283b6872e0fcaac
Laurent Rous <rouslaurent@gmail.com>
2019-11-05 13:28:36 +0100

    formulaire inscription

--- src/main/java/com/wildcodeschool/whereismyband/controller/ConnectedController.java
+++ src/main/java/com/wildcodeschool/whereismyband/controller/ConnectedController.java
@@ -1,19 +1,40 @@
 package com.wildcodeschool.whereismyband.controller;
 
+import com.wildcodeschool.whereismyband.entity.Musician;
+import com.wildcodeschool.whereismyband.repository.MusicianRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConnectedController {
+
+    private MusicianRepository repository = new MusicianRepository();
+
     @GetMapping("/profil-utilisateur")
     public String toProfile() {
         return "userProfile";
     }
 
     @PostMapping("/recherche")
-    public String toSearch() {
+    public String toSearch(Model model,
+                           @RequestParam String password,
+                           @RequestParam String newPassword,
+                           @RequestParam(required = false, defaultValue = "") String alias,
+                           @RequestParam String userMail,
+                           @RequestParam String postcode,
+                           @RequestParam(required = false, defaultValue = "") String bio,
+                           @RequestParam(required = false, defaultValue = "") String avatar,
+                           @RequestParam(required = false, defaultValue = "1111111") String availability,
+                           @RequestParam(required = false, defaultValue = "0")  int searchType,
+                           @RequestParam(required = false, defaultValue = "0")  int idSearch
+    ) {
+        //TODO vérifer password et confirm password
+        model.addAttribute("musician", repository.save(password, alias, userMail, postcode, bio, avatar,
+                availability, searchType, idSearch));
+
         return "search";
     }
 }
-
