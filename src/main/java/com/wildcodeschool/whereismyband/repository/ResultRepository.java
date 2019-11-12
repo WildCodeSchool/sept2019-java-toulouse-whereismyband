724a725d4da0c705ccc749180fb4a6a9b075b635
Mrrrrrggllup <54888723+Mrrrrrggllup@users.noreply.github.com>
2019-11-12 17:31:15 +0100

    Update ResultRepository.java

--- src/main/java/com/wildcodeschool/whereismyband/repository/ResultRepository.java
+++ src/main/java/com/wildcodeschool/whereismyband/repository/ResultRepository.java
@@ -1,7 +1,6 @@
 package com.wildcodeschool.whereismyband.repository;
 
 import com.wildcodeschool.whereismyband.entity.Result;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
